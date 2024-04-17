1679e4d4f829e4f1fb4f0086693d0c7ade550988
YossefMagdy3 <yossefshebl3@gmail.com>
2024-04-17 13:42:35 +0200

    SubjectValidator done ISA

--- Grading_systen_team5/src/main/java/org/example/SubjectValidator.java
+++ Grading_systen_team5/src/main/java/org/example/SubjectValidator.java
@@ -1,7 +1,7 @@
 package org.example;
 
 class SubjectValidator {
-    private final Subject subject ;
+    private final Subject subject;
     public static final String ERROR_MAX_DEGREE = "Error: Max degree must be one of the following values: 100.\n";
     public final static String STRING_EMPTY = "Error: Subject Name is empty.\n";
     public final static String SUBJECT_NAME_SPACE = "Error: Subject name starts with a space.\n";
@@ -19,54 +19,65 @@ class SubjectValidator {
         this.subject = subject;
     }
 
-    public static String checkSubjectName(String s)
-    {
-        if(s.length()==0) return STRING_EMPTY;
-        if(s.charAt(0)==' ') return SUBJECT_NAME_SPACE;
-        for(char c : s.toCharArray())
-        {
-            if(!Character.isAlphabetic(c)){
-                if(c!=' ')return SUBJECT_NAME_INVALID;
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public static String checkSubjectName(String s) {
+        if (s.length() == 0)
+            return STRING_EMPTY;
+        if (s.charAt(0) == ' ')
+            return SUBJECT_NAME_SPACE;
+        for (char c : s.toCharArray()) {
+            if (!Character.isAlphabetic(c)) {
+                if (c != ' ')
+                    return SUBJECT_NAME_INVALID;
             }
 
         }
         return "";
     }
 
-    public static String checkSubjectCode(String s){
-        if(s.length()!=6 && s.length()!=7) return SUBJECT_CODE_INVALID_LENGTH;
+    public static String checkSubjectCode(String s) {
+        if (s.length() != 6 && s.length() != 7)
+            return SUBJECT_CODE_INVALID_LENGTH;
 
-        for(int i = 0; i<3; i++){
-            if(!Character.isAlphabetic(s.charAt(i))) return SUBJECT_CODE_HAS_NUMBERS;
+        for (int i = 0; i < 3; i++) {
+            if (!Character.isAlphabetic(s.charAt(i)))
+                return SUBJECT_CODE_HAS_NUMBERS;
         }
-        for(int i = 3; i<6; i++){
-            if(Character.isAlphabetic(s.charAt(i))) return SUBJECT_CODE_HAS_LETTERS;
+        for (int i = 3; i < 6; i++) {
+            if (!Character.isDigit(s.charAt(i)))
+                return SUBJECT_CODE_HAS_LETTERS;
         }
-        if(s.length()==7 && s.charAt(s.length()-1) !='s') return SUBJECT_CODE_END;
+        if (s.length() == 7 && s.charAt(s.length() - 1) != 's')
+            return SUBJECT_CODE_END;
 
         return "";
     }
 
-    public static String checkMaxDegree(int degree)
-    {
-        if(degree!=100) return ERROR_MAX_DEGREE;
-        else return "";
+    public static String checkMaxDegree(int degree) {
+        if (degree != 100)
+            return ERROR_MAX_DEGREE;
+        else
+            return "";
     }
 
-
-    public static String checkSubjectData(Subject subject)
-    {
+    public static String checkSubjectData(Subject subject) {
         SubjectValidator validator = new SubjectValidator(subject);
         return validator.checkSubjectData();
     }
 
     public String checkSubjectName() {
         String s = subject.getName();
-        if (s.length() == 0) return STRING_EMPTY;
-        if (s.charAt(0) == ' ') return SUBJECT_NAME_SPACE;
+        if (s.length() == 0)
+            return STRING_EMPTY;
+        if (s.charAt(0) == ' ')
+            return SUBJECT_NAME_SPACE;
         for (char c : s.toCharArray()) {
             if (!Character.isAlphabetic(c)) {
-                if (c != ' ') return SUBJECT_NAME_INVALID;
+                if (c != ' ')
+                    return SUBJECT_NAME_INVALID;
             }
         }
         return "";
@@ -74,26 +85,32 @@ class SubjectValidator {
 
     public String checkSubjectCode() {
         String s = subject.getCode();
-        if (s.length() != 6 && s.length() != 7) return SUBJECT_CODE_INVALID_LENGTH;
+        if (s.length() != 6 && s.length() != 7)
+            return SUBJECT_CODE_INVALID_LENGTH;
 
         for (int i = 0; i < 3; i++) {
-            if (!Character.isAlphabetic(s.charAt(i))) return SUBJECT_CODE_HAS_NUMBERS;
+            if (!Character.isAlphabetic(s.charAt(i)))
+                return SUBJECT_CODE_HAS_NUMBERS;
         }
         for (int i = 3; i < 6; i++) {
-            if (Character.isAlphabetic(s.charAt(i))) return SUBJECT_CODE_HAS_LETTERS;
+            if (!Character.isDigit(s.charAt(i)))
+                return SUBJECT_CODE_HAS_LETTERS;
         }
-        if (s.length() == 7 && s.charAt(s.length() - 1) != 's') return SUBJECT_CODE_END;
+        if (s.length() == 7 && s.charAt(s.length() - 1) != 's')
+            return SUBJECT_CODE_END;
 
         return "";
     }
 
     public String checkMaxDegree() {
         int degree = subject.getFullMark();
-        if (degree != 100) return ERROR_MAX_DEGREE;
-        else return "";
+        if (degree != 100)
+            return ERROR_MAX_DEGREE;
+        else
+            return "";
     }
-    public String checkSubjectData()
-    {
+
+    public String checkSubjectData() {
         String result = "";
         result += checkSubjectCode(subject.getCode());
         result += checkSubjectName(subject.getName());
--- Grading_systen_team5/src/test/java/org/example/SubjectValidatorTest.java
+++ Grading_systen_team5/src/test/java/org/example/SubjectValidatorTest.java
@@ -1,18 +1,174 @@
 package org.example;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 class SubjectValidatorTest {
+    private SubjectValidator validatorEmpty;
+    private SubjectValidator validatorInitialized;
 
     @BeforeEach
     void setUp() {
+        String subjectData = "Software Testing,CSE333,100";
+        Subject subject = new Subject(subjectData);
+        validatorInitialized = new SubjectValidator(subject);
     }
 
     @AfterEach
     void tearDown() {
     }
+
+    @BeforeAll
+    void subjectValidatorObj() {
+        validatorEmpty = new SubjectValidator();
+
+    }
+
+    @Test
+    void testEmptyConstructor() {
+        assertNotNull(validatorEmpty);
+    }
+
+    @Test
+    void testInitializingConstructor() {
+        assertNotNull(validatorInitialized);
+    }
+
+    @Test
+    void testGetSubjectEmptyConstructor() {
+        assertNull(validatorEmpty.getSubject());
+    }
+
+    @Test
+    void testGetSubjectInitializingConstructor() {
+        assertNotNull(validatorInitialized.getSubject());
+    }
+
+    @Test
+    void testCheckSubjectName1() {
+
+        assertEquals("", validatorInitialized.checkSubjectName());
+    }
+
+    @Test
+    void testCheckSubjectName2() {
+        validatorInitialized.getSubject().setName("testing");
+        assertEquals("", validatorInitialized.checkSubjectName());
+    }
+
+    @Test
+    void testCheckSubjectName3() {
+        validatorInitialized.getSubject().setName("     ");
+        assertEquals(SubjectValidator.SUBJECT_NAME_SPACE, validatorInitialized.checkSubjectName());
+    }
+
+    @Test
+    void testCheckSubjectName4() {
+        validatorInitialized.getSubject().setName(" testing");
+        assertEquals(SubjectValidator.SUBJECT_NAME_SPACE, validatorInitialized.checkSubjectName());
+    }
+
+    @Test
+    void testCheckSubjectName5() {
+        validatorInitialized.getSubject().setName("testing123");
+        assertEquals(SubjectValidator.SUBJECT_NAME_INVALID, validatorInitialized.checkSubjectName());
+    }
+
+    @Test
+    void testCheckSubjectName6() {
+        validatorInitialized.getSubject().setName("test#ing$#");
+        assertEquals(SubjectValidator.SUBJECT_NAME_INVALID, validatorInitialized.checkSubjectName());
+    }
+
+    @Test
+    void testCheckSubjectCode1() {
+
+        assertEquals("", validatorInitialized.checkSubjectCode());
+    }
+
+    @Test
+    void testCheckSubjectCode2() {
+        validatorInitialized.getSubject().setCode("CSE333s");
+        assertEquals("", validatorInitialized.checkSubjectCode());
+    }
+
+    @Test
+    void testCheckSubjectCode3() {
+        validatorInitialized.getSubject().setCode("CS333s");
+        assertEquals(SubjectValidator.SUBJECT_CODE_HAS_NUMBERS, validatorInitialized.checkSubjectCode());
+    }
+
+    @Test
+    void testCheckSubjectCode4() {
+        validatorInitialized.getSubject().setCode("CSE33s");
+        assertEquals(SubjectValidator.SUBJECT_CODE_HAS_LETTERS, validatorInitialized.checkSubjectCode());
+    }
+
+    @Test
+    void testCheckSubjectCode5() {
+        validatorInitialized.getSubject().setCode("CS33s");
+        assertEquals(SubjectValidator.SUBJECT_CODE_INVALID_LENGTH, validatorInitialized.checkSubjectCode());
+    }
+
+    @Test
+    void testCheckSubjectCode6() {
+        validatorInitialized.getSubject().setCode("CSE4333s");
+        assertEquals(SubjectValidator.SUBJECT_CODE_INVALID_LENGTH, validatorInitialized.checkSubjectCode());
+    }
+
+    @Test
+    void testCheckSubjectCode7() {
+        validatorInitialized.getSubject().setCode("CSE4333");
+        assertEquals(SubjectValidator.SUBJECT_CODE_END, validatorInitialized.checkSubjectCode());
+    }
+
+    @Test
+    void testCheckSubjectCode8() {
+        validatorInitialized.getSubject().setCode("CSE433E");
+        assertEquals(SubjectValidator.SUBJECT_CODE_END, validatorInitialized.checkSubjectCode());
+    }
+
+    @Test
+    void testCheckSubjectCode9() {
+        validatorInitialized.getSubject().setCode("CSE433#");
+        assertEquals(SubjectValidator.SUBJECT_CODE_END, validatorInitialized.checkSubjectCode());
+    }
+
+    @Test
+    void testCheckSubjectCode10() {
+        validatorInitialized.getSubject().setCode("CSE43@s");
+        assertEquals(SubjectValidator.SUBJECT_CODE_HAS_LETTERS, validatorInitialized.checkSubjectCode());
+    }
+
+    @Test
+    void testCheckSubjectCode11() {
+        validatorInitialized.getSubject().setCode("CS@433s");
+        assertEquals(SubjectValidator.SUBJECT_CODE_HAS_NUMBERS, validatorInitialized.checkSubjectCode());
+    }
+
+    @Test
+    void testCheckMaxDegree1() {
+
+        assertEquals("", validatorInitialized.checkMaxDegree());
+    }
+
+    @Test
+    void testCheckMaxDegree2() {
+        validatorInitialized.getSubject().setFullMark(90);
+        assertEquals(SubjectValidator.ERROR_MAX_DEGREE, validatorInitialized.checkMaxDegree());
+    }
+
+    @Test
+    void testCheckMaxDegree3() {
+        validatorInitialized.getSubject().setFullMark(120);
+        assertEquals(SubjectValidator.ERROR_MAX_DEGREE, validatorInitialized.checkMaxDegree());
+    }
+
 }
