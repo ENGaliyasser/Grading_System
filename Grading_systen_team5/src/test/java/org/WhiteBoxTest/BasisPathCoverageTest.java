8b7a9bb018cd3cd70e45c1e414e555fe10a6f832
Donia Hany <doniahanyy312@gmai.com>
2024-05-13 23:51:31 +0300

    added

---
+++ Grading_systen_team5/src/test/java/org/WhiteBoxTest/BasisPathCoverageTest.java
@@ -0,0 +1,91 @@
+package org.WhiteBoxTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.example.InputHandler;
+import org.example.Subject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class BasisPathCoverageTest {
+    private ByteArrayOutputStream out;
+
+    @BeforeEach
+    void Init() {
+        out = new ByteArrayOutputStream();
+        PrintStream print = new PrintStream(out);
+        System.setOut(print);
+    }
+
+    // Test Case 1: File is empty
+    @Test
+    public void test1() {
+        String filename = "C:/Users/carol/Downloads/FinalProject/FinalProject/src/test/java/org/example/InputEmpty.txt";
+        InputHandler inputHandler = new InputHandler();
+        Subject sub = inputHandler.read(filename);
+
+        String actual = out.toString().trim();
+        String expected = "No subject data found. Subject data must be in the first line.";
+        assertEquals(expected, actual);
+    }
+
+    // Test Case 2: Valid subject data
+    @Test
+    public void test2() {
+        String filename = "C:/Users/carol/Downloads/FinalProject/FinalProject/src/test/java/org/example/InputValid.txt";
+        InputHandler inputHandler = new InputHandler();
+        Subject sub = inputHandler.read(filename);
+
+        assertEquals("Software testing", sub.getName());
+        assertEquals("CSE333", sub.getCode());
+        assertEquals(100, sub.getFullMark());
+    }
+
+    // Test Case 3: Subject data but no Student data
+    @Test
+    public void test3() {
+        String filename = "C:/Users/carol/Downloads/FinalProject/FinalProject/src/test/java/org/example/InputSubjectNoStudents.txt";
+        InputHandler inputHandler = new InputHandler();
+        Subject sub = inputHandler.read(filename);
+
+        assertEquals(0, sub.getStudents().size());
+    }
+
+    // Test Case 4: Subject Data not in first line
+    @Test
+    public void test4() {
+        String filename = "C:/Users/carol/Downloads/FinalProject/FinalProject/src/test/java/org/example/InputSubjectNotInFirstLine.txt";
+        InputHandler inputHandler = new InputHandler();
+        Subject sub = inputHandler.read(filename);
+
+        String actual = out.toString().trim();
+        String expected = "No subject data found. Subject data must be in the first line.";
+        assertEquals(expected, actual);
+    }
+
+    // Test Case 5: Valid subject data but wrong student data
+    @Test
+    public void test5() {
+        String filename = "C:/Users/carol/Downloads/FinalProject/FinalProject/src/test/java/org/example/InputCorrectSubjectWrongStudents.txt";
+        InputHandler inputHandler = new InputHandler();
+        Subject sub = inputHandler.read(filename);
+
+        assertEquals("Software testing", sub.getName());
+        assertEquals("CSE333", sub.getCode());
+        assertEquals(100, sub.getFullMark());
+        assertEquals(3, sub.getStudents().size());
+    }
+
+    // Test Case 6: File does not exist
+    @Test
+    public void test6() {
+        String filename = "InputNonExistent.txt";
+        InputHandler inputHandler = new InputHandler();
+        Subject sub = inputHandler.read(filename);
+
+        assertEquals(null, sub);
+    }
+}
---
+++ Grading_systen_team5/src/test/java/org/WhiteBoxTest/StatementCoverageTest.java
@@ -0,0 +1,81 @@
+package org.WhiteBoxTest;
+
+import org.example.Student;
+import org.example.StudentValidator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatementCoverageTest {
+
+    // Test case for an empty student name
+    @Test
+    void test1() {
+        Student student = new Student("", "12345678", 10, 8, 15, 50);
+        String expected = StudentValidator.STRING_EMPTY;
+        String actual = StudentValidator.checkStudentName(student);
+        assertEquals(expected, actual);
+    }
+
+    // Test case for a student name starting with a space
+    @Test
+    void test2() {
+        Student student = new Student(" Ali", "12345678", 10, 8, 15, 50);
+        String expected = StudentValidator.STUDENT_NAME_SPACE;
+        String actual = StudentValidator.checkStudentName(student);
+        assertEquals(expected, actual);
+    }
+
+    // Test case for a student name containing non-alphabetic characters
+    @Test
+    void test3() {
+        Student student = new Student("3abdelrahman", "12345678", 10, 8, 15, 50);
+        String expected = StudentValidator.STUDENT_NAME_INVALID;
+        String actual = StudentValidator.checkStudentName(student);
+        assertEquals(expected, actual);
+    }
+
+    // Test case for a valid student name
+    @Test
+    void test4() {
+        Student student = new Student("Youssef", "12345678", 10, 8, 15, 50);
+        String expected = "";
+        String actual = StudentValidator.checkStudentName(student);
+        assertEquals(expected, actual);
+    }
+
+    // Test case for a student number containing non-digit characters
+    @Test
+    void test5() {
+        Student student = new Student("Donia", "12345a78", 10, 8, 15, 50);
+        String expected = StudentValidator.STUDENT_NUMBER_CONTAIN;
+        String actual = StudentValidator.checkStudentNumber(student);
+        assertEquals(expected, actual);
+    }
+
+    // Test case for a student number not ending with an alphabet/digit
+    @Test
+    void test6() {
+        Student student = new Student("Noha", "1234567&", 10, 8, 15, 50);
+        String expected = StudentValidator.STUDENT_NUMBER_END;
+        String actual = StudentValidator.checkStudentNumber(student);
+        assertEquals(expected, actual);
+    }
+
+    // Test case for a student number with invalid length
+    @Test
+    void test7() {
+        Student student = new Student("Carol", "123456789", 10, 8, 15, 50);
+        String expected = StudentValidator.STUDENT_NUMBER_INVALID_LENGTH;
+        String actual = StudentValidator.checkStudentNumber(student);
+        assertEquals(expected, actual);
+    }
+
+    // Test case for a valid student number
+    @Test
+    void test8() {
+        Student student = new Student("Ali", "12345678", 10, 8, 15, 50);
+        String expected = "";
+        String actual = StudentValidator.checkStudentNumber(student);
+        assertEquals(expected, actual);
+    }
+}
